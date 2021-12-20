cfb83ac489c6e96bb64213bbf2410c5880f6b2f9
aallppxx <tg://@aallppxx>
2021-12-20 08:01:54 +0000

    removed unnecessary file

--- api/v1/pdo.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-//require_once 'settings.php';
-
-header('Content-type: application/json');
-
-$pdo = new PDO('mysql:host=127.0.0.1;dbname=testTask', 'root', 'toor');
-$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
